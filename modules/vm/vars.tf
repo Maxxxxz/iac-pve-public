84ffd5d15205236a9ea500af4dbc0cc10c9f2439
Michael Cooper <mc454916@ohio.edu>
2026-08-14 13:03:33 -0400

    Build out vm module

--- modules/vm/vars.tf
+++ modules/vm/vars.tf
@@ -1,15 +1,94 @@
-variable "pve_api_url" {
-  description = "The URL of the Proxmox API endpoint."
-  type        = string
+variable vm_name {
+  description = "var.vm_name"
+  type = string
+  validation {
+    condition = length(var.vm_name) > 0
+    error_message = "A 'vm_name' must be specified."
+  }
 }
 
-variable "pve_username" {
-  description = "The username for Proxmox authentication."
-  type        = string
+variable node_name {
+  description = "var.node_name"
+  type = string
+  validation {
+    condition = length(var.node_name) > 0
+    error_message = "A 'node_name' must be specified."
+  }
 }
 
-variable "pve_password" {
-  description = "The password for Proxmox authentication."
-  type        = string
-  sensitive   = true
+variable vm_description {
+  description = "Description of the virtual machine."
+  type = string
+  default = "Managed by OpenTofu"
+}
+
+variable cpu_cores {
+  description = "var.cpu_cores"
+  type = number
+  validation {
+    condition = var.cpu_cores > 0
+    error_message = "The number of 'cpu_cores' must be greater than 0."
+  }
+}
+
+variable cpu_sockets {
+  description = "var.cpu_sockets"
+  type = number
+  validation {
+    condition = var.cpu_sockets > 0
+    error_message = "The number of 'cpu_sockets' must be greater than 0."
+  }
+}
+
+variable cpu_architecture {
+  description = "var.cpu_architecture"
+  type = string
+  default = "x86_64"
+}
+
+variable cpu_type {
+  description = "The CPU type to use for the virtual machine. 'host' offers the best performance by using host pass-through."
+  type = string
+  default = "host"
+}
+
+variable memory_size_mb {
+  description = "The amount of memory to allocate to the virtual machine in MB."
+  type = number
+  validation {
+    condition = var.memory_size_mb > 0
+    error_message = "The 'memory_size_mb' must be greater than 0."
+  }
+}
+
+variable disk_datastore_id {
+  description = "var.disk_datastore_id"
+}
+
+variable disk_interface {
+  description = "var.disk_interface"
+}
+
+variable disk_import_from {
+  description = "The source of the disk image to import from PVE."
+  validation {
+    condition = length(var.disk_import_from) > 0
+    error_message = "The path in 'disk_import_from' must be specified."
+  }
+}
+
+variable ipv4_address {
+  description = "The IPv4 address to assign to the virtual machine."
+  type = string
+}
+
+variable user_account_username {
+  description = "The username for the default user account to be created on the virtual machine."
+  type = string
+}
+
+variable user_account_password {
+  description = "The password for the default user account to be created on the virtual machine."
+  type = string
+  sensitive = true
 }
---
+++ modules/vm/vm.tf
@@ -0,0 +1,36 @@
+resource "proxmox_virtual_environment_vm" "this" {
+    name = var.vm_name
+    node_name = var.node_name
+    description = join("\n\n", [var.vm_description, "(Managed by OpenTofu)"])
+    cpu {
+        cores = var.cpu_cores
+        sockets = var.cpu_sockets
+        architecture = var.cpu_architecture
+        type = var.cpu_type
+    }
+
+    memory {
+        dedicated = var.memory_size_mb
+    }
+
+    disk {
+        datastore_id = var.disk_datastore_id
+        interface = var.disk_interface
+        import_from = var.disk_import_from
+    }
+
+    initialization {
+        ip_config {
+            ipv4 {
+                address = var.ipv4_address
+            }
+        }
+
+        user_account {
+            username = var.user_account_username
+            password = var.user_account_password
+        }
+    }
+
+
+}
