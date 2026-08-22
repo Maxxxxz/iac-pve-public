18120c49e92998071fae9d6114b2024b50b1a046
Michael Cooper <mc454916@ohio.edu>
2026-08-21 23:20:57 -0400

    Consolidate input into vm_config object

--- modules/vm/providers.tf
+++ modules/vm/providers.tf
@@ -1,8 +1,8 @@
 terraform {
-    required_providers {
-        proxmox = {
-            source  = "bpg/proxmox"
-            version = "0.111.1"
-        }
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.111.1"
     }
+  }
 }
--- modules/vm/vm.tf
+++ modules/vm/vm.tf
@@ -1,37 +1,37 @@
 resource "proxmox_virtual_environment_vm" "this" {
-    name = var.vm_name
-    node_name = var.node_name
-    description = join("\n\n", [var.vm_description, "(Managed by OpenTofu)"])
-    tags = var.vm_tags
-    cpu {
-        cores = var.cpu_cores
-        sockets = var.cpu_sockets
-        architecture = var.cpu_architecture
-        type = var.cpu_type
+  name        = var.vm_name
+  node_name   = var.node_name
+  description = join("\n\n", [var.vm_config.vm_description, "(Managed by OpenTofu)"])
+  tags        = var.vm_config.vm_tags
+  cpu {
+    cores        = var.vm_config.cpu_cores
+    sockets      = var.vm_config.cpu_sockets
+    architecture = var.vm_config.cpu_architecture
+    type         = var.vm_config.cpu_type
+  }
+
+  memory {
+    dedicated = var.vm_config.memory_size_mb
+  }
+
+  disk {
+    datastore_id = var.vm_config.disk_datastore_id
+    interface    = var.vm_config.disk_interface
+    import_from  = var.vm_config.disk_import_from
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = var.vm_config.ipv4_address
+      }
     }
 
-    memory {
-        dedicated = var.memory_size_mb
+    user_account {
+      username = var.vm_config.user_account_username
+      password = var.user_account_password
     }
-
-    disk {
-        datastore_id = var.disk_datastore_id
-        interface = var.disk_interface
-        import_from = var.disk_import_from
-    }
-
-    initialization {
-        ip_config {
-            ipv4 {
-                address = var.ipv4_address
-            }
-        }
-
-        user_account {
-            username = var.user_account_username
-            password = var.user_account_password
-        }
-    }
+  }
 
 
 }
